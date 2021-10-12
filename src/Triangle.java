b3f56e690706afb57881a30d900586eb2b7b3f04
Zeino Madikizela <zeinosite@gmail.com>
2021-10-12 14:35:55 +0200

    Class Triangle: Added : Fields : side1, side2, side3 Constructors : edited all to include all sides Methods : Getters and Setters for all sides. perimeter, area and setSides methods
    Fields:
    For all three sides a field that is of type double was added
    
    Constructors:
    all Constructors initiated the sides to be 0
    
    Methods:
    Usual getters and setters. The perimeter method takes in all three sides as arguments and one method to set all sides that also takes in three arguments.
    
    Other methods that don't take in any arguments, for the perimeter and area, these two methods assume that the sides have been assigned to the correct values.

--- src/Triangle.java
+++ src/Triangle.java
@@ -2,6 +2,9 @@ public class Triangle extends Point
 {
     double height;
     double length;
+    double side1;
+    double side2;
+    double side3;
 
     //Constructors
     public Triangle()
@@ -9,6 +12,9 @@ public class Triangle extends Point
         super();
         this.height = 0;
         this.length = 0;
+        this.side1 = 0;
+        this.side2 = 0;
+        this.side3 = 0;
     }
 
     public Triangle(int xCoord, int yCoord)
@@ -17,6 +23,9 @@ public class Triangle extends Point
         this.height = 0;
         this.length = 0;
         this.zCoord = 0;
+        this.side1 = 0;
+        this.side2 = 0;
+        this.side3 = 0;
     }
 
     public Triangle(int xCoord, int yCoord, int zCoord)
@@ -24,6 +33,10 @@ public class Triangle extends Point
         super(xCoord, yCoord, zCoord);
         this.height = 0;
         this.length = 0;
+        this.side1 = 0;
+        this.side2 = 0;
+        this.side3 = 0;
+
     }
 
     public Triangle(int xCoord, int yCoord, int zCoord, double length, double height)
@@ -31,9 +44,10 @@ public class Triangle extends Point
         super(xCoord, yCoord, zCoord);
         this.height = height;
         this.length = length;
+        this.side1 = 0;
+        this.side2 = 0;
+        this.side3 = 0;
     }
-
-
 
     //Getters and Setters
     public double getHeight() {
@@ -50,6 +64,56 @@ public class Triangle extends Point
 
     public void setLength(double length) {
         this.length = length;
+    }
+
+    public double getSide1() {
+        return side1;
+    }
+
+    public void setSide1(double side1) {
+        this.side1 = side1;
+    }
+
+    public double getSide2() {
+        return side2;
+    }
+
+    public void setSide2(double side2) {
+        this.side2 = side2;
+    }
+
+    public double getSide3() {
+        return side3;
+    }
+
+    public void setSide3(double side3) {
+        this.side3 = side3;
+    }
+
+    public void setSides(double side1, double side2, double side3 )
+    {
+        this.side1 = side1;
+        this.side2 = side2;
+        this.side3 = side3;
+    }
+
+    public double perimeter()
+    {
+        return (side1 + side2 + side3);
+    }
+
+    public double perimeter(double side1, double side2, double side3)
+    {
+        return side1 + side2 + side3;
+    }
+
+    public double area ()
+    {
+        return (height + length)/2;
+    }
+    public double area(double height, double length)
+    {
+        return (height+length)/2;
     }
 
 }
