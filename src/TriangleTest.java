174c994bef6df14d580a3c652492ea462f0d2c00
Zeino Madikizela <zeinosite@gmail.com>
2021-10-13 13:51:54 +0200

    Class : TriangleTest
    1. Added the setSides methods to set the sides
    2. Set the coordinates for the shape
    3. Tested the setCoordinates method
    4. Set the sides with the setSides method
    5. Tested the getters for the coordinates and sides
    6. Tested the area and perimeter method.

--- src/TriangleTest.java
+++ src/TriangleTest.java
@@ -4,20 +4,41 @@ class TriangleTest {
     {
         Triangle myTriangle = new Triangle();
 
-        //Print current coordinates
+        //Print current coordinates                                                                                     //Passed
         System.out.println("These are the coordinates of the shape : " + myTriangle.showCoordinates());
 
         //Print the sides                                                                                               //Passed
         System.out.print("These are the sides of the shape : " + myTriangle.showSides());
 
-        //Test the setters for coordinates                                                                              //Passed
+        //Test the setters for coordinates and sides                                                                    //Passed
         myTriangle.setxCoord(1);
         myTriangle.setyCoord(3);
         myTriangle.setzCoord(5);
         System.out.println("\nThese are the new points : " + myTriangle.showCoordinates());
 
+        myTriangle.setSide1(5);
+        myTriangle.setSide2(4);
+        myTriangle.setSide3(6);
+
+        //Test the setCoordinates method                                                                                //Passed
+        myTriangle.setCoordinates(2,4,1);
+        System.out.println("\nThese are the new points : " + myTriangle.showCoordinates());
+
+        //Set the sides                                                                                                 //Passed
+        myTriangle.setSides(3,4,5);
 
 
+        //Test the getters                                                                                              //Passed
+        System.out.println("The x coordinate is : "+ myTriangle.getxCoord() );
+        System.out.println("The y coordinate is : "+ myTriangle.getyCoord() );
+        System.out.println("The z coordinate is : "+ myTriangle.getzCoord() );
+        System.out.println("Side 1 is : "+ myTriangle.getSide1() );
+        System.out.println("Side 2 is : "+ myTriangle.getSide2() );
+        System.out.println("Side 3 is : "+ myTriangle.getSide3() );
+
+        //Testing the area and perimeter                                                                                //Passed
+        System.out.println("The Perimeter is : " + myTriangle.perimeter());
+        System.out.println("The Area is : " + myTriangle.area() );
     }
 
 }
