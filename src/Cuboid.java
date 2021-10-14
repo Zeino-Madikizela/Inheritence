7a087a7a504f07e43dd3e1a9af167e55d74f53f2
Zeino Madikizela <zeinosite@gmail.com>
2021-10-14 19:54:41 +0200

    Class Cuboid:
    Created the class, added the field height and all other fields are inherited from the class Rectangle
    Created two constructors

---
+++ src/Cuboid.java
@@ -0,0 +1,24 @@
+public class Cuboid extends Rectangle
+{
+    //Fields
+    double height;
+
+
+    //Constructors
+
+    public Cuboid()
+    {
+        super();
+        this.height = 0;
+
+    }
+
+    public Cuboid(double length, double breadth, double height)
+    {
+        super(length, breadth);
+        this.height = height;
+    }
+
+
+
+}
