3cf36f9077c36787c194c7d1012c940cac934263
Zeino Madikizela <zeinosite@gmail.com>
2021-10-24 09:10:03 +0200

    Class TrianglularPrism :
    Added the getHeight and setHeight method
    Added the setSidesAndHeight method, that takes in all the side of the base triangle and the height of the prism
    
    Added the calcVolume method

--- src/TrianglularPrism.java
+++ src/TrianglularPrism.java
@@ -17,7 +17,23 @@ public class TrianglularPrism extends Triangle
         this.height = height;
     }
 
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
     //Methods
+
+    public void setSidesAndHeight(double side1, double side2, double side3, double height)
+    {
+        this.side1 = side1;
+        this.side2 = side2;
+        this.side3 = side3;
+        this.height = height;
+
+    }
 
     public String showDimensions()
     {
@@ -30,6 +46,11 @@ public class TrianglularPrism extends Triangle
         areaOfTriangles = 2*super.calcArea();
         areaOfRectangles = height*(side1 + side2 + side3);
         return areaOfTriangles + areaOfRectangles;
+    }
+
+   public double calcVolume()
+    {
+        return super.calcArea()*this.height;
     }
 
 }
